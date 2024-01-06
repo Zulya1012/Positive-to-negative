0e1815f50f1496d145e3f4aa0ac0c07c2be449ba
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-06 21:44:03 +0400

    Update forr.java

--- forr.java
+++ forr.java
@@ -1,9 +1,8 @@
-public class forr{
-public static void main(String[] arg){
-for (int i=5; i>=-5; i--){
+public class forr {
+  public static void main(String[] arg) {
+    for (int i = 5; i >= -5; i--) {
 
-
-System.out.println(i +" ");
-}
-}
+      System.out.println(i + " ");
+    }
+  }
 }
